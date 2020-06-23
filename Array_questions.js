184e9ea216711eee39124f7978feeedff2121df0
VeronicaChenyuk <beresneva.veronika@mail.ru>
2020-06-23 13:57:12 +0300

    +4 Soluthions

--- Array_questions.js
+++ Array_questions.js
@@ -48,4 +48,42 @@ const rotate = function (nums, k) {
   }
   return nums
 }
-console.log('Third ---',rotate([1, 2, 3, 4, 5, 6, 7], 3))
+console.log('Third ---', rotate([1, 2, 3, 4, 5, 6, 7], 3))
+
+// contains dublicate
+
+const containsDuplicate = function (nums) {
+  let count = {};
+  for (let i = 0; i < nums.length; i++) {
+    count[nums[i]] = 1 + (count[nums[i]] || 0);
+  }
+  let found = Object.keys(count).filter(function (key) {
+    return count[key] > 1
+  })
+  return found.length ? true : false;
+}
+console.log(containsDuplicate([1, 2, 5, 4]))
+
+// Single number
+
+let singleNumber = (nums) => nums.reduce((c, n) => c ^= n)
+
+console.log(singleNumber([2, 5, 2]))
+
+// Intersection of Two Arrays II
+
+let intersect = function (nums1, nums2) {
+  return nums1.filter(x => nums2.includes(x))
+}
+console.log(intersect([1, 2, 6, 5], [8, 9, 7, 5]));
+
+// Plus One
+
+let plusOne = function (digits) {
+  for (let i = 0; i < digits.length; i++) {
+    let last = digits[digits.length - 1] + 1
+    result = digits.slice(0, digits.length - 1).concat(last);
+    return result
+  }
+}
+console.log(plusOne([1, 2, 3, 4, 5]));
